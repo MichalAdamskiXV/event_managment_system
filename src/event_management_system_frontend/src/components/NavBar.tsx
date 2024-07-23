9f8e2af4698009e490bc0630e38a23208c9ae439
MichalAdamskiXV <madamskixv@gmail.com>
2024-07-23 20:58:50 +0200

    update NavBar links

--- src/event_management_system_frontend/src/components/NavBar.tsx
+++ src/event_management_system_frontend/src/components/NavBar.tsx
@@ -1,12 +1,13 @@
 import { Link, Outlet } from "react-router-dom"
-import { FaHome, FaHotjar, FaSearch } from "react-icons/fa";
+import { FaHome, FaHotjar, FaSearch, FaHeart } from "react-icons/fa";
 
 const NavBar = () => {
 
-    const sidebarLinks = [
+    const navbarLinks = [
         { id: 1, title: 'Home', path: '/', icon: <FaHome /> },
         { id: 2, title: 'Search', path: '/search', icon: <FaSearch /> },
         { id: 3, title: 'Hot Events', path: '/hotEvents', icon: <FaHotjar /> },
+        { id: 4, title: "Favorited Events", path: '/favoritedEvents', icon: <FaHeart /> },
     ];
 
     return (
@@ -14,7 +15,7 @@ const NavBar = () => {
             <nav className="w-[100%] p-3 bg-dark flex justify-between items-center">
                 <ul className="flex gap-6">
                     {
-                        sidebarLinks.map(({ icon, id, path, title }) => (
+                        navbarLinks.map(({ icon, id, path, title }) => (
                             <Link key={id} to={path}>
                                 <li className="hover:bg-body rounded-[8px] flex items-center text-light font-extrabold text-2xl p-3">
                                     {icon}
